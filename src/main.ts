70d95bcd4deeae18fda0e09c673d05c946c8ab84
Carlo Valdivia Luna <cvaldivialu@unsa.edu.pe>
2025-06-01 04:23:34 -0500

    agrega swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,33 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './app/app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // API Principal
+  const config = new DocumentBuilder()
+    .setTitle('Projecto Asistencia Citesoft')
+    .setDescription('Documentation of the entire project API')
+    .setVersion('1.0')
+    .addTag('citesoft')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('swagger', app, documentFactory, {
+    jsonDocumentUrl: 'swagger/json',
+  });
+
+  //Configuración de prefijo global
+  app.setGlobalPrefix('api');
+
+  const port = process.env.PORT || 3000;
+
+  await app.listen(port, () => {
+    console.log(`Application is running on: http://localhost:${port}/api`);
+  });
 }
-bootstrap();
+
+bootstrap().catch((err) => {
+  console.error('Error starting the app:', err);
+});
