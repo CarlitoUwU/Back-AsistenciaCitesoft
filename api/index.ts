55a14ae9f00e673f2e2b1dd27c5db7be0dbc69b1
Carlo Valdivia Luna <cvaldivialu@unsa.edu.pe>
2025-06-02 15:05:47 -0500

    corrige api/index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,27 +1,24 @@
 // api/index.ts
-import { createServer, proxy } from '@vendia/serverless-express';
-import { Callback, Context, Handler } from 'aws-lambda';
-import { AppModule } from '../src/app.module';
+import createServer from '@vendia/serverless-express';
+import { Handler } from 'aws-lambda';
+import { AppModule } from '../src/app/app.module';
 import { NestFactory } from '@nestjs/core';
 import { ExpressAdapter } from '@nestjs/platform-express';
-import * as express from 'express';
+import express from 'express';
 
-let server: Handler;
+let cachedServer: Handler;
 
 async function bootstrap(): Promise<Handler> {
   const expressApp = express();
   const adapter = new ExpressAdapter(expressApp);
   const app = await NestFactory.create(AppModule, adapter);
   await app.init();
-
-  return createServer(expressApp);
+  return createServer({ app: expressApp }); 
 }
 
-export const handler = async (
-  event: any,
-  context: Context,
-  callback: Callback,
-) => {
-  server = server ?? (await bootstrap());
-  return proxy(server, event, context, 'PROMISE').promise;
+export const handler: Handler = async (event, context) => {
+  if (!cachedServer) {
+    cachedServer = await bootstrap();
+  }
+  return cachedServer(event, context, () => {});
 };
